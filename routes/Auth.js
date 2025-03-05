e5b2ef38738d1b2605e2a0a16a1dffb234f81498
shivamguptarps <shivamgeniusgupta@gmail.com>
2025-03-05 19:01:53 +0530

    Added files

---
+++ routes/Auth.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { GetUser, Login, Register } from '../controllers/Auth.js'
+import { upload } from '../middlewares/Mluter.js'
+
+const AuthRoutes=express.Router()
+
+AuthRoutes.post('/register',upload.single('profile'),Register)
+AuthRoutes.post('/login', Login)
+AuthRoutes.get('/get_user', GetUser)
+AuthRoutes.get("hey",(req,res)=>{
+    res.send("hello");
+})
+export default AuthRoutes
